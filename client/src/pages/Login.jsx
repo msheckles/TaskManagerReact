b0527848cd3df4a43a57d1655cbd8c9bd135adf6
yal <yalda.shabani27@gmail.com>
2024-07-06 03:10:35 +0330

    add welcome section

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React , { useEffect }from "react";  
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const user = ""
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+
+      const navigate = useNavigate();
+      useEffect(() => {
+        user && navigate("/dashboard");
+      }, [user]);
   return (
-    <div>
-      Login
+    <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
+        <div iv className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
+            {/* left side */}
+            <div className='h-full w-full lg:w-3/3 flex flex-col items-center justify-center'>
+            <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
+            <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600'>
+             ladies and gentelmen , we prsent our final web project!
+            </span>
+            <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
+              <span>  welcome to </span>
+              <span>Task Manager</span>
+            </p>
+
+            <div className='cell'>
+              <div className='circle rotate-in-up-left'></div>
+            </div>
+          </div>
+            </div>
+            
+            {/* right side */}
+            <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'></div>
+        </div>
     </div>
   )
 }
