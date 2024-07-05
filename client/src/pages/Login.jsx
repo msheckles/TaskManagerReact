095b6e8233656a8dc3dafc4395f4d4b42f2e4695
yal <yalda.shabani27@gmail.com>
2024-07-06 01:55:38 +0330

    fix typo

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Login=()=>{
-    return(
-        <dir>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </dir>
+const Login = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     )
-}
-
-export default Login
+  }
+  
+  export default Login
