c529c2645be9e3e64e8fd339844a27f3e53c77de
yal <yalda.shabani27@gmail.com>
2024-07-06 01:51:59 +0330

    modify app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Login from './pages/Login'
 
 function App() {
+  
 
-
-  return (
-    <Login></Login>
-  )
+  return <Login />
 }
 
 export default App
