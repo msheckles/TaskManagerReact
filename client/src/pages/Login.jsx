adc7a4b7c2ee421271db3823fa482c2e8f331a06
yal <yalda.shabani27@gmail.com>
2024-07-06 03:52:50 +0330

    finish login page!

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React , { useEffect }from "react";  
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-
+import Textbox from "../components/Textbox";
+import Button from "../components/Button";
 const Login = () => {
     const user = ""
     const {
@@ -11,6 +12,10 @@ const Login = () => {
       } = useForm();
 
       const navigate = useNavigate();
+
+      const submitHandler = async (data) => {
+        console.log("submit");
+      };
       useEffect(() => {
         user && navigate("/dashboard");
       }, [user]);
@@ -18,7 +23,7 @@ const Login = () => {
     <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
         <div iv className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
             {/* left side */}
-            <div className='h-full w-full lg:w-3/3 flex flex-col items-center justify-center'>
+            <div className='h-full w-full lg:w-3/4 flex flex-col items-center justify-center'>
             <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
             <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600'>
              ladies and gentelmen , we prsent our final web project!
@@ -35,7 +40,47 @@ const Login = () => {
             </div>
             
             {/* right side */}
-            <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'></div>
+            <div className='w-full md:w-1/4 p-4 md:p-1 flex flex-col justify-center items-center'>
+                <form  onSubmit={handleSubmit(submitHandler)}
+            className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14'
+          >
+            <div className='flex flex-col gap-y-5'>
+              <Textbox
+                placeholder='email@example.com'
+                type='email'
+                name='email'
+                label='Email Address'
+                className='w-full rounded-full'
+                register={register("email", {
+                  required: "Email Address is required!",
+                })}
+                error={errors.email ? errors.email.message : ""}
+              />
+              <Textbox
+                placeholder='your password'
+                type='password'
+                name='password'
+                label='Password'
+                className='w-full rounded-full'
+                register={register("password", {
+                  required: "Password is required!",
+                })}
+                error={errors.password ? errors.password.message : ""}
+              />
+
+              <span className='text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer'>
+                Forget Password?
+              </span>
+
+              <Button
+                type='submit'
+                label='Submit'
+                className='w-full h-10 bg-blue-700 text-white rounded-full'
+              />
+            </div>
+
+                </form>
+            </div>
         </div>
     </div>
   )
